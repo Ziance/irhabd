4da1a7c826af76bb3930cf4537df305d8997971e
zt developer <ztdeveloper7>
2024-01-11 14:21:13 +0530

    removed log

--- webapp/src/pages/mapView.js
+++ webapp/src/pages/mapView.js
@@ -1,74 +1,71 @@
 // MapView.js
 import React, { useEffect, useState, useRef } from "react";
 import { Card, Container, Row, Col } from "reactstrap";
-import GoogleMapReact from "google-map-react";
 import LocationDropDown from "../components/common/locationDropDown";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchDevices, selectLocation } from "../redux/slices/locationSlice";
 import { toast } from "react-toastify";
-import InfoWindow from "./InfoWindow";
-import Marker from "./marker";
+import { MarkerClusterer } from "@googlemaps/markerclusterer";
 
 const MapWrapper = ({ data }) => {
-  const [infoWindow, setInfoWindow] = useState(null);
-  const [center, setCenter] = useState({ lat: 0, lng: 0 });
-  const [zoom] = useState(3);
-
+  const mapRef = useRef(null);
   useEffect(() => {
-    if (window.google && window.google.maps) {
-      if (data.length > 0) {
-        const bounds = new window.google.maps.LatLngBounds();
-        data.forEach((marker) => {
-          bounds.extend(
-            new window.google.maps.LatLng(marker.latitude, marker.longitude)
-          );
+    const google = window.google;
+    let map = mapRef.current;
+
+    if (map) {
+      const bounds = new google.maps.LatLngBounds();
+      const myLatlng = new google.maps.LatLng(data[0]?.lat, data[0]?.lng); // Center the map on the first marker
+      const mapOptions = {
+        zoom: 12,
+        center: myLatlng,
+        scrollwheel: false,
+        zoomControl: true,
+        styles: [],
+      };
+
+      map = new google.maps.Map(map, mapOptions);
+
+      const infoWindows = [];
+
+      data.forEach((markerData, index) => {
+        const marker = new google.maps.Marker({
+          position: new google.maps.LatLng(markerData?.lat, markerData?.lng),
+          map: map,
+          animation: google.maps.Animation.DROP,
+          title: markerData?.title,
         });
-        const newCenter = {
-          lat: (bounds.getNorthEast().lat() + bounds.getSouthWest().lat()) / 2,
-          lng: (bounds.getNorthEast().lng() + bounds.getSouthWest().lng()) / 2,
-        };
-        setCenter(newCenter);
-      }
+
+        bounds.extend(marker.position);
+
+        const infoWindow = new google.maps.InfoWindow({
+          content: markerData?.content,
+        });
+
+        infoWindows.push(infoWindow);
+
+        google.maps.event.addListener(marker, "click", () => {
+          infoWindows.forEach((iw) => iw.close()); // Close any open info windows
+          infoWindow.open(map, marker);
+        });
+      });
+
+      map.fitBounds(bounds); // Zoom to fit all data
+
+      const markerCluster = new MarkerClusterer(map, data, {
+        imagePath:
+          "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
+      });
     }
   }, [data]);
 
-  const handleMarkerClick = (marker) => {
-    setInfoWindow(marker);
-  };
-
-  const handleMapClick = (event) => {
-    if (event.target && !event.target.classList.contains("marker")) {
-      setInfoWindow(null);
-    }
-  };
-
   return (
-    <div style={{ height: "500px", width: "100%" }}>
-      <GoogleMapReact
-        bootstrapURLKeys={{ key: "AIzaSyDnrrB6Nu2FFFrM6eO-oQXFu_Y9yHp2cL4" }}
-        defaultCenter={center}
-        defaultZoom={zoom}
-        onClick={handleMapClick}
-        yesIWantToUseGoogleMapApiInternals={true}
-      >
-        {data.map((marker) => (
-          <Marker
-            key={marker.id}
-            lat={marker.latitude}
-            lng={marker.longitude}
-            onClick={() => handleMarkerClick(marker)}
-          />
-        ))}
-      </GoogleMapReact>
-      {infoWindow && (
-        <InfoWindow onClose={() => setInfoWindow(null)}>
-          <div>
-            <h2>{"infoWindow.title"}</h2>
-            <p>{"infoWindow.description"}</p>
-          </div>
-        </InfoWindow>
-      )}
-    </div>
+    <div
+      style={{ height: "600px" }}
+      className="map-canvas"
+      id="map-canvas"
+      ref={mapRef}
+    />
   );
 };
 
@@ -110,11 +107,26 @@ const MapView = () => {
         <Row>
           <Col>
             <Card className="shadow border-0">
-              <MapWrapper
-                data={location?.devices.filter(
-                  (marker) => marker.latitude && marker.longitude
-                )}
-              />
+              {(location?.devices?.length > 0 && (
+                <MapWrapper
+                  data={location.devices
+                    .filter((marker) => marker.latitude && marker.longitude)
+                    .map((marker) => {
+                      if (marker.latitude && marker.longitude) {
+                        return {
+                          lat: marker.latitude,
+                          lng: marker.longitude,
+                          title: marker.station,
+                          content: `<div class="info-window-content"><h2>${marker.station}</h2><p>Latitude: ${marker.latitude}</p><p>Longitude: ${marker.longitude}</p></div>`,
+                        };
+                      } else return;
+                    })}
+                />
+              )) || (
+                <div className="text-muted text-center">
+                  Map can't be loaded
+                </div>
+              )}
             </Card>
           </Col>
         </Row>
